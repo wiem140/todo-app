371120033cb585bfe6f372f0814fa02d3f99cb38
wiem140 <84313071+wiem140@users.noreply.github.com>
2021-08-12 10:29:06 +0100

    Delete src directory

--- src/I.S/Action-type/action-type.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TASK = "ADD_TASK";
-
-export const EDIT_TASK = "EDIT_TASK";
-export const DONE_TASK = "DONE_TASK";
-export const DELETE_TASK ="DELETE_TASK";
--- src/I.S/Reducer/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import TaskReducer from "./taskReducer";
-
-const rootReducer = combineReducers ({task:TaskReducer});
-
-export default rootReducer ;
